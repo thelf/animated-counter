cfaa7ef78d47d3de2e38eadfcf377ad7604f1ba3
thomas.helfrich <info@helfrich-media.de>
2023-06-07 14:02:29 +0200

    Funktionen ausgelagert.

---
+++ inc/all.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/db-connect.inc.php';
+require_once __DIR__ . '/functions.inc.php';
+
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-
-function e($html) {
-    return htmlspecialchars($html, ENT_QUOTES, 'UTF-8', true);
-}
+require_once __DIR__ . '/inc/all.php';
 
 $data = [
     'Besucher' => 21035,
